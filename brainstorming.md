c0427d481cdcc94407f1a75cddd5252d044a5c94
Daiva Krisnu Faisa Addia <z5508443@ad.unsw.edu.au>
2024-08-27 19:13:57 +1000

    First iteration of CAN parser + brainstorming notes

--- brainstorming.md
+++ brainstorming.md
@@ -3,7 +3,18 @@
 This file is used to document your thoughts, approaches and research conducted across all tasks in the Technical Assessment.
 
 ## Firmware
+The first thought that comes to mind is to use an existing CAN parsing library. Although this might be the easier way, this does introduce significant
+overhead to the parser program. Based on my limited understanding of the software and how RB23's data parsing actually works, if this parser ran directly
+in RB23's embedded system (maybe for live low latency CAN data analyzing?) it would be beneficial if it were as small as possible to maximise memory use and efficiency.
+Similarly, I will try to minimise the use of external libraries for this exact reason.
+
+First step would be to grab the CAN id and check that it matches ECU_WheelSpeed's DBC id and ignore the line if it doesn't.
+
+
+
 
 ## Telemetry
+
+Looks like the app crashes when a random additional '}' appears in the end of the data.
 
 ## Cloud
---
+++ firmware/parser.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+int main () {
+    std::ifstream Dump("dump.log");
+    std::string line;
+
+    const std::string frameId = "705";
+
+    while (getline(Dump, line)) {
+        // get ID
+        if (line.substr(26, 3).compare(frameId) != 0) {
+            continue;
+        }
+
+        //grab timestamp
+        std::cout << line.substr(0, 19) << ": WheelSpeedFR: ";
+
+        std::string data = line.substr(30, 16);
+        uint64_t dataInInt;
+
+        sscanf(data.c_str(), "%lx", &dataInInt);
+
+        // 16 bits long starting from position 0
+        // grab 1 byte each
+        int mask = 0xFF;
+        uint msb = (dataInInt >> 48) & mask;
+        uint fr = msb << 8;
+        uint lsb = (dataInInt >> 56) & mask;
+        fr |= lsb;
+
+        msb = (dataInInt >> 16) & mask;
+        uint rr = msb << 8;
+        lsb = (dataInInt >> 24) & mask;
+        rr |= lsb;
+
+        std::cout << std::fixed << std::setprecision(1);
+
+        double frOutput = fr * 0.1;
+        std::cout << frOutput << '\n';
+
+        double rrOutput = rr * 0.1;
+        std::cout << line.substr(0, 19) << ": WheelSpeedRR: " << rrOutput << '\n';
+    }
+
+    Dump.close();
+}
